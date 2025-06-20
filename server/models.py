7e811dc7acb99429bd12153b25e915ef656614bf
Donald Kiprop <kipropdonald27@gmail.com>
2025-06-20 18:27:33 +0300

    modified models.py

--- server/models.py
+++ server/models.py
@@ -1,7 +1,14 @@
-from server.extensions import db, bcrypt
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
+# Initialize these here (assumes you're importing them into app.py and using init_app if needed)
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+
+# User model
 class User(db.Model):
     __tablename__ = 'users'
+    __table_args__ = {'extend_existing': True}
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, nullable=False)
@@ -9,7 +16,7 @@ class User(db.Model):
     image_url = db.Column(db.String)
     bio = db.Column(db.String)
 
-    recipes = db.relationship("Recipe", backref="user", lazy=True)
+    recipes = db.relationship('Recipe', backref='user', lazy=True)
 
     @property
     def password_hash(self):
@@ -22,8 +29,10 @@ class User(db.Model):
     def authenticate(self, password):
         return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
 
+# Recipe model
 class Recipe(db.Model):
     __tablename__ = 'recipes'
+    __table_args__ = {'extend_existing': True}
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
